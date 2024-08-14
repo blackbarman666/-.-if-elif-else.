5c55f29e2ddab9b33b9ecfd34260f397f2c2cef4
blackbarman666 <mihaman05@gmail.com>
2024-08-14 17:11:46 +0300

    Add files via upload

---
+++ домашка 8 модуль 2 операторы и циклы.py
@@ -0,0 +1,12 @@
+first = input('Первое число: ')
+second = input('Второе число: ')
+third = input('Третье число: ')
+
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+elif first != second != third:
+    print(0)
+
+
