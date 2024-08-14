ce1dbd7e226c8a613d5d5955e779e35b8234b8c7
blackbarman666 <mihaman05@gmail.com>
2024-08-14 16:58:57 +0300

    14.08.2024

---
+++ домашка 8 модуль 2 операторы и циклы.py
@@ -0,0 +1,18 @@
+first=(int(input()))
+second=(int(input()))
+third=(int(input()))
+
+a=first
+b=second
+c=third
+
+if a==b==c:
+    print(3)
+elif a==b!=c:
+    print(2)
+elif a!=b!=c:
+    print(0)
+
+
+
+
